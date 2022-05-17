2bb0c977c759c80fc307f715c457a98680a0754d
Ali Taghawi <marshaltaghawi@gmail.com>
2022-05-17 17:51:44 +0430

    creating search capability!

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -8,36 +8,38 @@ import Loader from './Loader';
 import Coin from './Coin';
 
 const Landing = () => {
-
-  const [state, setState] = useState([])
+  const [state, setState] = useState([]);
+  const [search , setSearch] = useState("");
 
   useEffect(() => {
     const fetchAPI = async () => {
       setState(await getData())
     }
-
     fetchAPI();
-  }, [])
+  }, []);
+  
+  const searchHandeler = event => {
+    setSearch(event.target.value)
+  };
+
+  const searchedCoins = state.filter(coin => coin.name.toLowerCase().includes(search.toLowerCase()));
 
   return (
     <>
-      <input type='text' placeholder='Search' />
-      {
-        state.length ?
-          <div>
-            {
-              state.map(coin => <Coin 
-                key={coin.id}
-                name={coin.name}
-                image={coin.image}
-                symbol={coin.symbol}
-                price={coin.current_price}
-                marketCap={coin.market_cap}
-                priceChange={coin.price_change_percentage_24h}
-              />)
-            }
-          </div> :
-          <Loader />
+      <input type='text' placeholder='Search' value={search} onChange={searchHandeler} />
+      {state.length ?
+        <div>
+          {searchedCoins.map(coin => <Coin 
+            key={coin.id}
+            name={coin.name}
+            image={coin.image}
+            symbol={coin.symbol}
+            price={coin.current_price}
+            marketCap={coin.market_cap}
+            priceChange={coin.price_change_percentage_24h}
+          />)}
+        </div> :
+        <Loader />
       }
     </>
   );
