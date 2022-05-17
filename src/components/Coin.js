251cc986181175c27d97f4386a3ead69982fecf9
Ali Taghawi <marshaltaghawi@gmail.com>
2022-05-17 17:34:54 +0430

    creating 'Loader.js' & 'Coin.js' and use them in landing.js

---
+++ src/components/Coin.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Coin = ({ name, image, symbol, price, marketCap, priceChange }) => {
+    return (
+        <div>
+            <img src={image} alt={name} />
+            <span>{name}</span>
+            <span>{symbol.toUpperCase()}</span>
+            <span>$ {price.toLocaleString()}</span>
+            <span>{priceChange.toFixed(2)}</span>
+            <span>$ {marketCap.toLocaleString()}</span>
+        </div>
+    );
+};
+
+export default Coin;
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,6 +3,10 @@ import React, {useEffect, useState} from 'react';
 //API
 import { getData } from '../services/api';
 
+//components
+import Loader from './Loader';
+import Coin from './Coin';
+
 const Landing = () => {
 
   const [state, setState] = useState([])
@@ -16,9 +20,26 @@ const Landing = () => {
   }, [])
 
   return (
-    <div>
-      
-    </div>
+    <>
+      <input type='text' placeholder='Search' />
+      {
+        state.length ?
+          <div>
+            {
+              state.map(coin => <Coin 
+                key={coin.id}
+                name={coin.name}
+                image={coin.image}
+                symbol={coin.symbol}
+                price={coin.current_price}
+                marketCap={coin.market_cap}
+                priceChange={coin.price_change_percentage_24h}
+              />)
+            }
+          </div> :
+          <Loader />
+      }
+    </>
   );
 };
 
